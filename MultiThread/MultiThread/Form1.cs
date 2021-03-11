c8af02d9129b2fa12405ac92ed75d8690d77c979
Davide Innocenti <d.innocenti@chemitec.it>
2021-03-11 17:31:10 +0100

    Aggiunto progetto multitrhread

---
+++ MultiThread/MultiThread/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThread
+{
+    public partial class Form1 : Form
+    {
+        Tests.ThreadClass tClass1;
+        Tests.ThreadClass tClass2;
+        Thread thread1;
+        Thread thread2;
+
+        public delegate void UpdateLog(string text);
+        public UpdateLog delegatoLog;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.delegatoLog = new UpdateLog(addToLog);
+
+            tClass1 = new Tests.ThreadClass("thread_1");
+            tClass2 = new Tests.ThreadClass("thread_2");
+            tClass1.log += new Tests.ThreadClass.updateText(log);
+            tClass2.log += new Tests.ThreadClass.updateText(log);
+
+            thread1 = new Thread(tClass1.DoWork);
+            thread2 = new Thread(tClass2.DoWork);
+
+        }
+
+        private void log(string text)
+        {
+           //delegatoLog(text);
+            Invoke(delegatoLog, new object[] { text });
+        }
+
+        public void addToLog(string text)
+        {
+            textBox1.Text += text + "\r\n";
+            this.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            thread1.Start();
+            thread2.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thread1.Abort();
+            thread2.Abort();
+        }
+    }
+}
---
+++ MultiThread/MultiThread/Tests/ThreadClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThread.Tests
+{
+
+    class ThreadClass
+    {
+        public delegate void updateText(string text);
+        public event updateText log;
+        private string text = "";
+
+        public ThreadClass(string _text)
+        {
+            this.text = _text;
+        }
+
+        public void DoWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+                //Console.WriteLine("Working thread...");
+                log(text);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
